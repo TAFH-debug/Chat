394c2eded2c67182cba3bab5694b09c7255ef943
TAFH-debug <taim20077@mail.ru>
2023-02-05 22:50:32 +0600

    aa

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ root = Tk()
 root.title("Chat")
 root.geometry("1000x800")
 
+nickname = ""
+
 def con():
     con_lbl1.pack()
     con_ip.pack()
@@ -28,6 +30,8 @@ def con_proc(ip, nick):
     con_nickname.pack_forget()
     con_btn.pack_forget()
     channel_name = connect(ip, nick)
+    global nickname
+    nickname = nick
     Channel(channels, channel_name).pack(fill=X)
 
 def crt_proc(chn, nick):
@@ -36,6 +40,9 @@ def crt_proc(chn, nick):
     crt_lbl2.pack_forget()
     crt_nickname.pack_forget()
     crt_btn.pack_forget()
+
+def send(txt):
+    Message(messages, nickname, txt).pack(fill=X, side=BOTTOM)
 
 
 menu = Menu(root, tearoff=0)
@@ -73,7 +80,7 @@ con_btn = Button(frame2, command=lambda: con_proc(con_ip.get(), con_nickname.get
 #message = Message(messages, "TAFH", "Hellow")
 lbl = Label(frame3, text="Channels", font=("Helvetica", 32), bg="#3D550C", fg="#ECF87F")
 entry = Entry(enter_fr, font=("Helvetica", 20), width=40)
-btn = Button(enter_fr, text="Send", width=20, bg="#4c9a15", activebackground="#60a231", borderwidth=0)
+btn = Button(enter_fr, text="Send", width=20, bg="#4c9a15", activebackground="#60a231", borderwidth=0, command=lambda: send(entry.get()))
 
 frame1.pack(side=LEFT, fill=Y)
 frame2.pack(side=LEFT, fill=BOTH, expand=1)
